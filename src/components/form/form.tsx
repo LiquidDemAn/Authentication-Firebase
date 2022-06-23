c30e1fc1100af480739afe96624bf9b1a943aa5a
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-06-23 17:00:22 +0300

    fixed styles

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,3 +1,4 @@
+import './form.scss';
 import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
@@ -19,22 +20,22 @@ export const FormComponent = ({
 
 	return (
 		<Form>
-			{title && <h2 className='fw-bold fs-30 mb-4'>{title}</h2>}
+			{title && <h2 className='form__title'>{title}</h2>}
 
-			<Form.Group className='mb-3' controlId='formEmail'>
+			<Form.Group className='form-group' controlId='formEmail'>
 				<Form.Label>Email address</Form.Label>
 				<Form.Control
-					className='p-2'
+					className='form-group__control'
 					type='email'
 					ref={emailRef}
 					placeholder='Enter email'
 					required
 				/>
 			</Form.Group>
-			<Form.Group className='mb-3' controlId='formPassword'>
+			<Form.Group className='form-group' controlId='formPassword'>
 				<Form.Label>Password</Form.Label>
 				<Form.Control
-					className='p-2'
+					className='form-group__control'
 					type='password'
 					ref={passwordRef}
 					placeholder='Password'
@@ -43,20 +44,22 @@ export const FormComponent = ({
 			</Form.Group>
 
 			{formId === 'login' && (
-				<div className='d-flex justify-content-between mt-3 mb-3'>
+				<div className='form-settings'>
 					<Form.Group
-						className='d-flex cursor-pointer gap-2'
+						className='form-settings__remember'
 						controlId='formRemeber'
 					>
 						<Form.Check type='checkbox' />
 						<Form.Label>Remember me</Form.Label>
 					</Form.Group>
-					<a href='/'>Forgot password?</a>
+					<a className='form-settings__forgot' href='/'>
+						Forgot password?
+					</a>
 				</div>
 			)}
 
 			<Button
-				className='w-100 mb-4'
+				className='form__btn'
 				variant='success'
 				onClick={() =>
 					handleClick(emailRef.current!.value, passwordRef.current!.value)
