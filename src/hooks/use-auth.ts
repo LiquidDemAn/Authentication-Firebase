c1d7f1648c2de9cbecc0ab5584405997b7942692
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-07-01 10:36:43 +0300

    rewritten useAuth hook for firebase

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,13 +1,38 @@
-import { getUser } from '../pages/services/selectors';
-import { useAppSelector } from './../store/hooks';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getUserEmail } from './../pages/services/selectors';
+import { useAppDispatch } from './../store/hooks';
+import { setUser } from '../pages/services/user.slice';
 
 export const useAuth = () => {
-	const { email, id, token } = useAppSelector(getUser);
+	const auth = getAuth();
+	const dispatch = useAppDispatch();
+	const userEmail = useSelector(getUserEmail);
+	const [isAuth, setIsAuth] = useState(true);
+
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			user.getIdToken().then((token) => {
+				if (!userEmail) {
+					dispatch(
+						setUser({
+							email: user.email,
+							id: user.uid,
+							token,
+						})
+					);
+				}
+			});
+
+			setIsAuth(true);
+		} else {
+			setIsAuth(false);
+		}
+	});
 
 	return {
-		isAuth: !!email,
-		email,
-		token,
-		id,
+		isAuth,
+		email: userEmail,
 	};
 };
