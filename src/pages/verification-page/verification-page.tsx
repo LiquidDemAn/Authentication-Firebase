8b1ff26a43a5622ff2421b57ec55088ba10ef0f6
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-07-02 21:53:33 +0300

    implemented logic of resend letter on email

--- src/pages/verification-page/verification-page.tsx
+++ src/pages/verification-page/verification-page.tsx
@@ -1,14 +1,49 @@
+import { useEffect } from 'react';
 import { getAuth, sendEmailVerification } from 'firebase/auth';
 import { Verification } from '../../components/verification';
+import { VerificationEnum } from '../../components/verification/verification';
 import { Wrapper } from '../../components/wrapper';
+import { FirebaseError } from 'firebase/app';
+import { useDispatch } from 'react-redux';
+import { setError } from '../services/user.slice';
+import { ErrorsEnum } from '../services/typedef';
+import { useAppSelector } from '../../store/hooks';
+import { getAuthStatus } from '../services/selectors';
+import { useNavigate } from 'react-router-dom';
 
 export const VerificationPage = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const auth = getAuth();
 	const user = auth.currentUser;
+	const isAuth = useAppSelector(getAuthStatus);
+
+	useEffect(() => {
+		if (isAuth === false) {
+			navigate('/login');
+		}
+	}, [isAuth, navigate]);
+
+	const resendHandle = () => {
+		if (user) {
+			sendEmailVerification(user, { url: 'http://localhost:3000/' })
+				.then(() => {
+					document.location.reload();
+				})
+				.catch((error: FirebaseError) => {
+					const errorCode = error.code;
+					console.log(errorCode);
+					dispatch(setError(errorCode as ErrorsEnum));
+				});
+		}
+	};
 
 	return (
 		<Wrapper>
-			<Verification />
+			<Verification
+				type={VerificationEnum.Register}
+				resendHandle={resendHandle}
+			/>
 		</Wrapper>
 	);
 };
