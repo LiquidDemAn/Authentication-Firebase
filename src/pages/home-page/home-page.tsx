f37fdf2b5265b28df52b2efd89ddd6fde94b3030
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-07-02 21:51:55 +0300

    changed logout button

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,29 +1,17 @@
-import { useEffect } from 'react';
 import './home-page.scss';
-import { signOut, getAuth } from 'firebase/auth';
-import { Button } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { Wrapper } from '../../components/wrapper';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { removeUser } from '../services/user.slice';
-import { FirebaseError } from 'firebase/app';
+import { useAppSelector } from '../../store/hooks';
+
 import { getAuthStatus } from '../services/selectors';
+import { LogOutButton } from '../../components/log-out-button';
 
 export const HomePage = () => {
 	const isAuth = useAppSelector(getAuthStatus);
 	const navigate = useNavigate();
-	const dispatch = useAppDispatch();
 	const auth = getAuth();
-
-	const logOut = () => {
-		signOut(auth)
-			.then(() => {
-				dispatch(removeUser());
-			})
-			.catch((error: FirebaseError) => {
-				console.error(`LogOut Error: ${error.code}`);
-			});
-	};
 
 	useEffect(() => {
 		if (isAuth === false) {
@@ -37,13 +25,7 @@ export const HomePage = () => {
 				<h2 className='home-page__title'>Welcome</h2>
 				<div className='home-page__content'>
 					<span className='home-page__email'>{auth.currentUser?.email}</span>
-					<Button
-						className='home-page__button'
-						variant='danger'
-						onClick={logOut}
-					>
-						Sign out
-					</Button>
+					<LogOutButton />
 				</div>
 			</div>
 		</Wrapper>
