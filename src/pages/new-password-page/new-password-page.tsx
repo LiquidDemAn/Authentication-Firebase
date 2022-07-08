6db1a14392c4fd78249c24e3105beaad10559c91
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-07-08 20:40:28 +0300

    Created use-query hook

--- src/pages/new-password-page/new-password-page.tsx
+++ src/pages/new-password-page/new-password-page.tsx
@@ -1,8 +1,19 @@
 import { FormTitle } from '../../components/common/form-title';
 import { Wrapper } from '../../components/common/wrapper';
 import { NewPasswordForm } from '../../components/new-password/new-password-form';
+import { useQuery } from '../../hooks/use-query';
+import { confirmPasswordReset, getAuth } from 'firebase/auth';
 
 export const NewPasswordPage = () => {
+	const query = useQuery();
+	const oobCode = query.get('oobCode');
+	const auth = getAuth();
+
+	const onSubmit = (
+		newPassword: string | null,
+		confirmNewPassword: string | null
+	) => {};
+
 	return (
 		<Wrapper>
 			<FormTitle>Reset Password Confirm</FormTitle>
